577f30e9f563d967ecf27ea736c9c3c864d09625
t-onyango <t.onyango@alustudent.com>
2022-10-27 21:05:55 +0200

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000;
 
 
 app.get('/', function(req, res){
-    res.json({slackUsername: 'topingstar', backend: true, age : 24, bio: 'I am Topister, 24 years old. Currently a computer science student at African Leadership University. I am more focused in Backend development.I am very much interested in learning new technology.'})
+    res.json({slackUsername: 'topingstar', backend: true, age : 24, bio: 'I am Topister, 24 years old. Currently a computer science student at African Leadership University. I am more focused in Backend development.I am very much interested in learning new technology and using them.'})
 
 })
 
